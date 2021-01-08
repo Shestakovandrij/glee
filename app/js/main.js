3e1917b54815f59cb026c1facc5ea934045992cf
Shestakovandrij <shestakovandrij@gmai.com>
2021-01-08 11:49:56 +0100

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,27 @@
 $(function(){
 
+  function scrollMenu() {
+    let menu = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+      if (pageYOffset >= document.documentElement.clientHeight / 15) {
+        menu.style['background-color'] = 'rgba(15, 10, 20, .5)'
+        menu.style['height'] = '120px'
+      } else {
+        menu.style['background-color'] = 'transparent'
+        menu.style['height'] = '104px'
+      }
+    });
+  }
+  scrollMenu();
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
+
+    onStart: function (data)  {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
     onChange: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
